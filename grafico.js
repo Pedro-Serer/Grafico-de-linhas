4783bc606e66dce604bcf77b599c20a97ccee4fe
Pedro-Serer <42715517+Pedro-Serer@users.noreply.github.com>
2019-02-25 04:44:14 -0300

    Create grafico.js

---
+++ grafico.js
@@ -0,0 +1,143 @@
+var namespaceSVG = "http://www.w3.org/2000/svg";
+        var text  = document.createElementNS(namespaceSVG, "text");
+        var path  = document.createElementNS(namespaceSVG, "path");
+        var texto = document.createTextNode("2000");
+
+        text.appendChild(texto);
+        text.setAttributeNS(null, "x", 0);
+        text.setAttributeNS(null, "y", 35);
+        path.setAttributeNS(null, "d", "M 0 35 q 2046 0 100 0");
+        path.setAttributeNS(null, "stroke", "black");
+        path.setAttribute(null, "stroke-width", "1");
+        path.setAttributeNS(null, "fill", "none");
+        path.setAttributeNS(null, "stroke-dasharray", "1,6,0");
+        document.getElementById("treino").appendChild(text);
+        document.getElementById("treino").appendChild(path);
+
+        var text1  = document.createElementNS(namespaceSVG, "text");
+        var path1  = document.createElementNS(namespaceSVG, "path");
+        var texto1 = document.createTextNode("1900");
+
+        text1.appendChild(texto1);
+        text1.setAttributeNS(null, "x", 0);
+        text1.setAttributeNS(null, "y", 70);
+        path1.setAttribute(null, "class", "a");
+        path1.setAttributeNS(null, "d", "M 0 70 q 2046 0 100 0");
+        path1.setAttributeNS(null, "stroke", "black");
+        path1.setAttributeNS(null, "stroke-width", "1");
+        path1.setAttributeNS(null, "fill", "none");
+        path1.setAttributeNS(null, "stroke-dasharray", "1,6,0");
+        document.getElementById("treino").appendChild(text1);
+        document.getElementById("treino").appendChild(path1);
+
+        var text3  = document.createElementNS(namespaceSVG, "text");
+        var path3  = document.createElementNS(namespaceSVG, "path");
+        var texto3 = document.createTextNode("1800");
+
+        text3.appendChild(texto3);
+        text3.setAttributeNS(null, "x", 0);
+        text3.setAttributeNS(null, "y", 105);
+        path3.setAttribute(null, "class", "a");
+        path3.setAttributeNS(null, "d", "M 0 105 q 2046 0 100 0");
+        path3.setAttributeNS(null, "stroke", "black");
+        path3.setAttributeNS(null, "stroke-width", "1");
+        path3.setAttributeNS(null, "fill", "none");
+        path3.setAttributeNS(null, "stroke-dasharray", "1,6,0");
+        document.getElementById('treino').appendChild(text3);
+        document.getElementById("treino").appendChild(path3);
+
+        var text4  = document.createElementNS(namespaceSVG, "text");
+        var path4  = document.createElementNS(namespaceSVG, "path");
+        var texto4 = document.createTextNode("1700");
+
+        text4.appendChild(texto4);
+        text4.setAttributeNS(null, "x", 0);
+        text4.setAttributeNS(null, "y", 140);
+        path4.setAttribute(null, "class", "a");
+        path4.setAttributeNS(null, "d", "M 0 140 q 2046 0 100 0");
+        path4.setAttributeNS(null, "stroke", "black");
+        path4.setAttributeNS(null, "stroke-width", "1");
+        path4.setAttributeNS(null, "fill", "none");
+        path4.setAttributeNS(null, "stroke-dasharray", "1,6,0");
+        document.getElementById('treino').appendChild(text4);
+        document.getElementById("treino").appendChild(path4);
+
+        var text5  = document.createElementNS(namespaceSVG, "text");
+        var path5  = document.createElementNS(namespaceSVG, "path");
+        var texto5 = document.createTextNode("1600");
+
+        text5.appendChild(texto5);
+        text5.setAttributeNS(null, "x", 0);
+        text5.setAttributeNS(null, "y", 175);
+        path5.setAttribute(null, "class", "a");
+        path5.setAttributeNS(null, "d", "M 0 175 q 2046 0 100 0");
+        path5.setAttributeNS(null, "stroke", "black");
+        path5.setAttributeNS(null, "stroke-width", "1");
+        path5.setAttributeNS(null, "fill", "none");
+        path5.setAttributeNS(null, "stroke-dasharray", "1,6,0");
+        document.getElementById('treino').appendChild(text5);
+        document.getElementById("treino").appendChild(path5);
+
+        var text6  = document.createElementNS(namespaceSVG, "text");
+        var path6  = document.createElementNS(namespaceSVG, "path");
+        var texto6 = document.createTextNode("1500");
+
+        text6.appendChild(texto6);
+        text6.setAttributeNS(null, "x", 0);
+        text6.setAttributeNS(null, "y", 210);
+        path6.setAttribute(null, "class", "a");
+        path6.setAttributeNS(null, "d", "M 0 210 q 2046 0 100 0");
+        path6.setAttributeNS(null, "stroke", "black");
+        path6.setAttributeNS(null, "stroke-width", "1");
+        path6.setAttributeNS(null, "fill", "none");
+        path6.setAttributeNS(null, "stroke-dasharray", "1,6,0");
+        document.getElementById('treino').appendChild(text6);
+        document.getElementById("treino").appendChild(path6);
+
+        var text7  = document.createElementNS(namespaceSVG, "text");
+        var path7  = document.createElementNS(namespaceSVG, "path");
+        var texto7 = document.createTextNode("1400");
+
+        text7.appendChild(texto7);
+        text7.setAttributeNS(null, "x", 0);
+        text7.setAttributeNS(null, "y", 245);
+        path7.setAttribute(null, "class", "a");
+        path7.setAttributeNS(null, "d", "M 0 245 q 2046 0 100 0");
+        path7.setAttributeNS(null, "stroke", "black");
+        path7.setAttributeNS(null, "stroke-width", "1");
+        path7.setAttributeNS(null, "fill", "none");
+        path7.setAttributeNS(null, "stroke-dasharray", "1,6,0");
+        document.getElementById('treino').appendChild(text7);
+        document.getElementById("treino").appendChild(path7);
+
+        var text8  = document.createElementNS(namespaceSVG, "text");
+        var path8  = document.createElementNS(namespaceSVG, "path");
+        var texto8 = document.createTextNode("1300");
+
+        text8.appendChild(texto8);
+        text8.setAttributeNS(null, "x", 0);
+        text8.setAttributeNS(null, "y", 280);
+        path8.setAttribute(null, "class", "a");
+        path8.setAttributeNS(null, "d", "M 0 280 q 2046 0 100 0");
+        path8.setAttributeNS(null, "stroke", "black");
+        path8.setAttributeNS(null, "stroke-width", "1");
+        path8.setAttributeNS(null, "fill", "none");
+        path8.setAttributeNS(null, "stroke-dasharray", "1,6,0");
+        document.getElementById('treino').appendChild(text8);
+        document.getElementById("treino").appendChild(path8);
+
+        var text9  = document.createElementNS(namespaceSVG, "text");
+        var path9  = document.createElementNS(namespaceSVG, "path");
+        var texto9 = document.createTextNode("1200");
+
+        text9.appendChild(texto9);
+        text9.setAttributeNS(null, "x", 0);
+        text9.setAttributeNS(null, "y", 315);
+        path9.setAttribute(null, "class", "a");
+        path9.setAttributeNS(null, "d", "M 0 315 q 2046 0 100 0");
+        path9.setAttributeNS(null, "stroke", "black");
+        path9.setAttributeNS(null, "stroke-width", "1");
+        path9.setAttributeNS(null, "fill", "none");
+        path9.setAttributeNS(null, "stroke-dasharray", "1,6,0");
+        document.getElementById('treino').appendChild(text9);
+        document.getElementById("treino").appendChild(path9);
